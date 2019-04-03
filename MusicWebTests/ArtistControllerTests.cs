1aecb3176130db8868c224e307b8e5b850c68b58
Joe B <jab6677@gmail.com>
2019-04-03 17:59:00 -0400

    Added a web test project and a very simple Selenium test. Took a while to figure out how to run it locally, but got it working.

---
+++ MusicWebTests/ArtistControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace MusicWebSiteTests
+{
+    public class Tests
+    {
+        String HostUrl;
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            //This can be run locally by hitting right click on the Music Web Site project, View, View in Browser
+            //then by running the tests here. 
+            HostUrl = "https://localhost:44367";
+            
+            driver = new EdgeDriver("C:\\");
+        }
+
+        [Test]
+        public void GetArtistTest()
+        {
+
+            driver.Url = HostUrl+"/artist/artists";
+            String pageTitleActual = driver.Title;
+            String pageTitleExpected = "Artists - MusicWebSite";
+
+            Assert.AreEqual(pageTitleExpected, pageTitleActual);
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Close();
+        }
+    }
+}
